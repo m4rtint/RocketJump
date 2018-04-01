9ce0966058e0731f718b248bb3ab828108fe8b74
Martin <m4rtin.t@gmail.com>
2018-03-31 18:18:08 -0700

    Added Fade script

---
+++ Assets/BlackScreenFade.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlackScreenFade : MonoBehaviour {
+
+	[SerializeField]
+	float TimeTakenToFade;
+
+	Image m_ScreenCover;
+
+	#region Mono
+	//If GameObject set as active in the beginning on scene Fade in
+	void Awake() {
+		m_ScreenCover = GetComponent<Image> ();
+	}
+
+	#endregion
+
+	#region active
+	public void StartFadeIn() {
+		m_ScreenCover.color = Color.black;
+		m_ScreenCover.CrossFadeAlpha (0, TimeTakenToFade, true);
+	}
+
+	public void StartFadeOut() {
+		m_ScreenCover.color = Color.clear;
+		m_ScreenCover.CrossFadeAlpha (1, TimeTakenToFade, true);
+	}
+	#endregion
+}
