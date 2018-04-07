a6dc567271048dcce2d394af11d10a851fd686f0
Martin <m4rtin.t@gmail.com>
2018-04-07 01:10:59 -0700

    Transition Cycle

--- Assets/Script/Manager/SceneTransitionManager.cs
+++ Assets/Script/Manager/SceneTransitionManager.cs
@@ -10,34 +10,36 @@ public class SceneTransitionManager : MonoBehaviour {
 	float m_TimeTakenToFade;
 
 	Image m_ScreenCover;
+	CanvasRenderer m_ScreenCover_Renderer;
 
 	#region Mono
 	//If GameObject set as active in the beginning on scene Fade in
 	void Awake() {
 		m_ScreenCover = GetComponent<Image> ();
+		m_ScreenCover_Renderer = GetComponent<CanvasRenderer> ();
 	}
 
 	void Start() {
-		FadeInIfNeeded ();
+		StartFadeIn ();
+		SetupColor ();
 	}
 
+	void SetupColor() {
+		m_ScreenCover.color = Color.black;
+	}
 	#endregion
 
 	#region Fade
-	void FadeInIfNeeded() {
-		if (StateManager.instance.CurrentState () == GameState.Game) {
-			StartFadeIn ();	
-		}
-	}
+
 	public void StartFadeIn() {
-		m_ScreenCover.color = Color.black;
+		m_ScreenCover_Renderer.SetAlpha (1);
 		m_ScreenCover.CrossFadeAlpha (0, m_TimeTakenToFade, true);
 		StartCoroutine ("DeactivateBlackScreen");
 	}
 
 	public void StartFadeOut() {
 		gameObject.SetActive (true);
-		m_ScreenCover.color = Color.clear;
+		m_ScreenCover_Renderer.SetAlpha (0);
 		m_ScreenCover.CrossFadeAlpha (1, m_TimeTakenToFade, true);
 		StartCoroutine ("StartChangeScene");
 	}
@@ -60,7 +62,7 @@ public class SceneTransitionManager : MonoBehaviour {
 	}
 
 	int GetNextSceneIndex() {
-		int numberOfScenes = SceneManager.sceneCount;
+		int numberOfScenes = SceneManager.sceneCountInBuildSettings;
 		int currentScene = SceneManager.GetActiveScene ().buildIndex;
 		currentScene++;
 
