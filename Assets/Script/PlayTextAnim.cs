3429a6fb741ddb58204358a4bccfde587b448e39
Martin <m4rtin.t@gmail.com>
2018-04-28 09:55:12 -0700

    Tap to play text fade anim

---
+++ Assets/Script/PlayTextAnim.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayTextAnim : MonoBehaviour {
+
+    [SerializeField]
+    float m_AlphaTime;
+    float m_CurrentTime = 0;
+
+    [SerializeField]
+    float m_TargetAlpha;
+
+    bool m_AlphaDirection = false;
+
+
+	// Update is called once per frame
+	void Update () {
+        m_CurrentTime += Time.deltaTime;
+        if (m_CurrentTime > m_AlphaTime){
+            m_AlphaDirection = !m_AlphaDirection;
+            m_CurrentTime = 0;
+            SwapAlpha(m_AlphaDirection);
+        }
+	}
+
+    void SwapAlpha(bool alpha){
+        float targetAlpha = 1;
+        if (alpha) {
+            targetAlpha = m_TargetAlpha;
+        }
+        GetComponent<Text>().CrossFadeAlpha(targetAlpha, m_AlphaTime, true);
+    }
+}
