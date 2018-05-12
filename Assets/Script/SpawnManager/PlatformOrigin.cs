81275c0c327ff711ef35c0f0ee51119e0f3d1667
Martin Tsang <m4rtin.t@gmail.com>
2018-05-12 09:39:22 -0700

    Bird sinks into nest at original

--- Assets/Script/SpawnManager/PlatformOrigin.cs
+++ Assets/Script/SpawnManager/PlatformOrigin.cs
@@ -10,11 +10,35 @@ public class PlatformOrigin : MonoBehaviour {
     [SerializeField]
     Sprite[] platforms;
 
+    [SerializeField]
+    GameObject m_PlatformObject;
+    [SerializeField]
+    GameObject m_PlatformOriginObject;
+
+    BoxCollider2D[] m_Colliders;
+
+    bool originalPassby = true;
+
+    private void Awake()
+    {
+        m_Colliders = new BoxCollider2D[] { m_PlatformObject.GetComponent<BoxCollider2D>(), m_PlatformOriginObject.GetComponent<BoxCollider2D>(), GetComponent<BoxCollider2D>() };
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.name == "EndSpawn")
         {
-            GetComponent<SpriteRenderer>().sprite = RandomizePlatform(); 
+            GetComponent<SpriteRenderer>().sprite = RandomizePlatform();
+
+            if (originalPassby)
+            {
+                for(int i = 0; i < m_Colliders.Length; i++) {
+                    Vector2 currentOffset = m_Colliders[i].offset;
+                    m_Colliders[i].offset = currentOffset + new Vector2(0, 0.25f);
+                    originalPassby = false;
+                }
+            }
+
         }
     }
 
