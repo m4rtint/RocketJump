1936ad835197a40136cba7e639140803957ba5b6
Martin <m4rtin.t@gmail.com>
2018-05-14 15:18:12 -0700

    Adding new sounds, set up for proper release

---
+++ Assets/Script/Rating.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rating : MonoBehaviour {
+
+	public void AppRate()
+	{      
+		#if UNITY_ANDROID
+		Application.OpenURL("market://details?id=com.MartinTsang.BirdyHop");
+        #elif UNITY_IPHONE
+		Application.OpenURL("itms-apps://itunes.apple.com/app/id1384642685");
+        #endif
+	}
+}
