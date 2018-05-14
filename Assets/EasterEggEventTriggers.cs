daa8f3b1c3772e6097d880ea37f2051989660339
Martin Tsang <m4rtin.t@gmail.com>
2018-05-13 23:03:21 -0700

    Event trigger bug fix

---
+++ Assets/EasterEggEventTriggers.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EasterEggEventTriggers : EventTrigger {
+
+    public override void OnInitializePotentialDrag(PointerEventData eventData)
+    {
+        AudioManager.instance.StartEasterEggActivation();
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        AudioManager.instance.StopEasterEggActivation();
+    }
+}
